55291b12e7221c13b1681b8f5518dc8080c86a42
Anil Cokbilir <anil-27@hotmail.de>
2021-07-28 12:27:16 +0200

    neu

--- backlog.js
+++ backlog.js
@@ -1,14 +1,27 @@
+loadAllTickets();
+
 function backlog() {
-    for (let i = 0; i < test1.length; i++) {
-        document.getElementById('test').innerHTML += `
+    for (let i = 0; i < AllTickets.length; i++) {
+        document.getElementById('backlog').innerHTML += `
         
-        <div>${test1[i]}</div>
+     
         
+        <div class="id width17">
+            <img src="./img/user.png">
+            <div class="name">
+                <p>Anil Cokbilir</p>
+                <p>Anil.Cokbilir@gmail.com</p>
+            </div>
+        </div>
+        <div id="title" class="width17">${AllTickets[i].Titel}</div>
+        <div class="width17">${AllTickets[i].DueDate}</div>
+
+        <p class="width17">${AllTickets[i].Category}</p>
+        <p class="width30">L${AllTickets[i].Decription}</p>
+   
         
         
     `
 
     };
 }
-
-let test1 = ['uno', 'dos'];
