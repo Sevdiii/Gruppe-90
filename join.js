2e677d734273a59318588f7243f531c01abc339c
Sevdi Azizi <you@example.com>
2021-08-02 19:34:21 +0200

    new

--- join.js
+++ join.js
@@ -1,12 +1,13 @@
 let AllTickets = [];
 
-
-
-
 function loadAllTickets() {
     let AllTicketsAsString = backend.getItem('AllTickets');
     AllTickets = JSON.parse(AllTicketsAsString) || []; // Wandelt text wieder in das alte JSON zurück
 
     console.log('loadedAllTickets', AllTickets);
 
+}
+
+function DeleteTickets() {
+    backend.deleteItem(key);
 }
